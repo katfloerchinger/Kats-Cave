0c5f38d2e9e4b2963bd8866e152469447da698f6
laurieryan <laurieryan@lauries-air.pk5001z>
2021-12-25 01:53:10 -0800

    calling first excel sheet

--- yassified.m
+++ yassified.m
@@ -1,2 +1,15 @@
 %author: Kat Floerchinger
-First personal project or whatevaaaa 
+%First personal project or whatevaaaa 
+%This script is the first and is only to see if I can get data from an
+%excel sheet
+
+%%Calling the sheet
+folder = '/Users/laurieryan/MATLAB/Personal Projects/funkyjazzy2022 github/Spreadsheets/';
+filename = [folder 'SampleDataforYassified.xlsx'];
+xlRange = 'A1:C10';
+[num,txt,raw] = xlsread(filename,xlRange);
+T = table(raw);
+uitable('Data',T{:,:},'ColumnName',T.Properties.VariableNames,...
+    'RowName',T.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+
+\
