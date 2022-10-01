102554d0b0df662da5d2b96ee23e690d47f5a856
sfmdf <sfmdf@DESKTOP-J0LQGE5.PK5001Z>
2022-09-30 20:03:02 -0700

    commiting

--- yassified.m
+++ yassified.m
@@ -5,11 +5,16 @@
 %excel sheet
 
 %%Calling the sheet
-folder = '/Users/laurieryan/MATLAB/Personal Projects/funkyjazzy2022 github/Spreadsheets/';
-filename = [folder 'SampleDataforYassified.xlsx'];
-xlRange = 'A1:C10';
-[num,txt,raw] = xlsread(filename,xlRange);
-T = table(raw);
-uitable('Data',T{:,:},'ColumnName',T.Properties.VariableNames,...
-    'RowName',T.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
-%%KATS BRANCH
+% folder = '/Users/laurieryan/MATLAB/Personal Projects/funkyjazzy2022 github/Spreadsheets/';
+% filename = [folder 'SampleDataforYassified.xlsx'];
+% xlRange = 'A1:C10';
+% [num,txt,raw] = xlsread(filename,xlRange);
+% T = table(raw);
+% uitable('Data',T{:,:},'ColumnName',T.Properties.VariableNames,...
+%     'RowName',T.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+%%KATS BRANCH testing
+clear all; close all; clc;
+
+A1 = 2.7;
+A2 = -8;
+A3 = pi;
